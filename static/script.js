3291359f084ad297736fb5aa6d6a9d323463eeae
Rohit Singh <forrohitsingh99@gmail.com>
2023-01-07 17:30:01 +0530

    7-1-22

---
+++ static/script.js
@@ -0,0 +1,24 @@
+let inputTextArea = document.getElementById("input-textarea");
+let characCount = document.getElementById("charac-count");
+
+let wordCount = document.getElementById("word-count");
+
+let insta = document.getElementById("instagram-count");
+
+let FB = document.getElementById("facebook-count");
+
+let Wapp = document.getElementById("whatsapp-count");
+
+inputTextArea.addEventListener("input", () => {
+    let temp = inputTextArea.value.length;
+    characCount.textContent = temp;
+    insta.textcontent = temp;
+
+    let txt = inputTextArea.value.trim();
+
+    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+
+   
+
+});
+
